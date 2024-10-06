e3d2ec5caa96b02347780bd8c5eaab5716c65c1f
Shantanu Khokrale <shantanukhokrale14@gmail.com>
2024-10-06 19:23:41 +0530

    Update Employee.java

--- Employee_Mgmt_System/src/main/java/com/cm/entities/Employee.java
+++ Employee_Mgmt_System/src/main/java/com/cm/entities/Employee.java
@@ -7,7 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Employee {
+public class Employee 
+{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,56 +24,69 @@ public class Employee {
 
 	private String password;
 
-	public int getId() {
+	public int getId() 
+	{
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(int id) 
+	{
 		this.id = id;
 	}
 
-	public String getName() {
+	public String getName() 
+	{
 		return name;
 	}
 
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
 
-	public String getEmail() {
+	public String getEmail() 
+	{
 		return email;
 	}
 
-	public void setEmail(String email) {
+	public void setEmail(String email) 
+	{
 		this.email = email;
 	}
 
-	public String getAddress() {
+	public String getAddress() 
+	{
 		return address;
 	}
 
-	public void setAddress(String address) {
+	public void setAddress(String address) 
+	{
 		this.address = address;
 	}
 
-	public String getGender() {
+	public String getGender() 
+	{
 		return gender;
 	}
 
-	public void setGender(String gender) {
+	public void setGender(String gender) 
+	{
 		this.gender = gender;
 	}
 
-	public String getPassword() {
+	public String getPassword() 
+	{
 		return password;
 	}
 
-	public void setPassword(String password) {
+	public void setPassword(String password) 
+	{
 		this.password = password;
 	}
 
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + ", gender="
 				+ gender + ", password=" + password + "]";
 	}
